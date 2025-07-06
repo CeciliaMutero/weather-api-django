f08dc4b052493d493e966f3aa4dd746b9ca0d555
Cecilia <cecilmutero66@gmail.com>
2025-07-06 04:48:55 +0300

    manually added gunicorn for render

--- core/settings.py
+++ core/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xew+*@kr$)njoj2%!yn7o@d9j(=3ywuv8y6&q#89d-vy*7dle5'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
